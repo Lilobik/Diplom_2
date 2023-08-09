90c30d4af21e3db6986f3628d5656443968a4bba
Pavel Larkin <lifepawel@yandex.ru>
2023-08-09 12:47:16 +0500

    udp OrderGetTest - add create new order

--- src/test/java/orderTest/OrderGetTest.java
+++ src/test/java/orderTest/OrderGetTest.java
@@ -12,6 +12,8 @@ import user.UserData;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasSize;
 
 public class OrderGetTest {
@@ -28,18 +30,24 @@ public class OrderGetTest {
         userApi.createUser(testUser);
         ValidatableResponse loginResponse = userApi.loginUser(UserData.from(testUser), "");
         accessToken = loginResponse.extract().path("accessToken");
-
-        // Создание заказа для авторизованного пользователя
-        orderApi.createOrderWithIngredients(List.of("61c0c5a71d1f82001bdaaa6f", "61c0c5a71d1f82001bdaaa70"), accessToken);
     }
 
     @Test
     @Description("Получение заказов авторизованным пользователем")
     public void testGetOrdersByAuthorizedUser() {
+
+        // Создание заказа для авторизованного пользователя
+        ValidatableResponse createOrderResponse = orderApi.createOrderWithIngredients(List.of("61c0c5a71d1f82001bdaaa6f", "61c0c5a71d1f82001bdaaa70"), accessToken);
+        createOrderResponse.statusCode(200);
+        createOrderResponse.body("success", equalTo(true));
+
+        // Получение заказов
         ValidatableResponse response = orderApi.getOrders(accessToken);
         response.statusCode(200);
         response.body("success", equalTo(true));
-        response.body("orders", hasSize(1));
+
+        // Проверка, что в полученных заказах есть созданный заказ
+        response.body("orders", hasItem(hasEntry("number", createOrderResponse.extract().path("order.number"))));
     }
 
     @Test
